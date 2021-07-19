683f411f681dfb7a3e5b92363dd3a42f2c196fcd
keshav211 <70699676+keshav211@users.noreply.github.com>
2021-07-19 21:54:28 +0530

    Add files via upload

---
+++ keshav(iitmod).py
@@ -0,0 +1,10 @@
+import  pywhatkit
+
+pywhatkit.text_to_handwriting('''
+1. Submit the URL of the colab notebook for “Week 9: Activity”. (This url should be
+added in the Google Sites course portfolio page created by you)
+2. Provide links to your Google Site in this form (Week 9 Activity - Form 1). Make
+sure that you provide View permission for the Google Site and Google colab file
+for ‘Everyone in Indian Institute of Technology Domain’
+3. Submit a screenshot of the portion of the website where you have explained the
+activity.''')
